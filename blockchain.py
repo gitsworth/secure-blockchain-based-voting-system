a5e8d400d4eb2a266b17b19ba6ec56e483f60129
gitsworth <satwiksnaik06@gmail.com>
2025-11-23 21:08:56 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,6 @@
-from datetime import datetime
+# blockchain.py
 import hashlib
+from datetime import datetime
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
@@ -7,22 +8,23 @@ class Block:
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
-        self.hash = self.compute_hash()
+        self.hash = self.calculate_hash()
 
-    def compute_hash(self):
+    def calculate_hash(self):
         block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
         return hashlib.sha256(block_string.encode()).hexdigest()
 
 class Blockchain:
     def __init__(self):
-        self.chain = []
-        self.create_genesis_block()
+        self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
-        genesis_block = Block(0, str(datetime.utcnow()), "Genesis Block", "0")
-        self.chain.append(genesis_block)
+        return Block(0, str(datetime.utcnow()), "Genesis Block", "0")
+
+    def get_last_block(self):
+        return self.chain[-1]
 
     def add_block(self, data):
-        previous_block = self.chain[-1]
-        new_block = Block(len(self.chain), str(datetime.utcnow()), data, previous_block.hash)
+        last_block = self.get_last_block()
+        new_block = Block(len(self.chain), str(datetime.utcnow()), data, last_block.hash)
         self.chain.append(new_block)
